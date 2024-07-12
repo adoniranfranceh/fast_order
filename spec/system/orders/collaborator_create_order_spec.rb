cfcf1963d6e5ee7ac3bbde43e6987348b37acb15
Adoniran <adoniranfranca@gmail.com>
2024-07-11 23:04:11 -0300

    WIP: Usuário cria pedido

---
+++ spec/system/orders/collaborator_create_order_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Colaborador cria novo pedido' do
+  it 'a partir da tela inicial' do
+    user = create :user, role: :collaborator
+
+    login_as user, scope: :user
+
+    click_on 'Novo Pedido'
+
+    select 'Local', from: 'Tipo de entrega'
+    fill_in 'Mesa', with: '5'
+
+    select
+  end
+end
