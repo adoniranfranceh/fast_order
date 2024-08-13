bab7e19a1cc1e557ce69cc8a0c26058b6b03e22f
Adoniran <adoniranfranca@gmail.com>
2024-08-13 17:54:51 -0300

    Adiciona esbuild watch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,10 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
+  config.assets.paths << Rails.root.join("app", "assets", "builds")
+  config.assets.debug = true
+  config.assets.compile = true
+
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
