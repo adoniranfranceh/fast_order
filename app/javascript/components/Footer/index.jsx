c3f915cd2f9f252748b2c195ee4ef87f381e72e4
Adoniran <adoniranfranca@gmail.com>
2024-10-07 18:18:56 -0300

    Remove twitter do rodapé

--- app/javascript/components/Footer/index.jsx
+++ app/javascript/components/Footer/index.jsx
@@ -4,11 +4,10 @@ import { StyledFooter, FooterText, SocialLink, SocialLinks } from './style'
 const Footer = () => {
   return (
     <StyledFooter>
-      <FooterText>&copy; 2024 Açaí Manager - Todos os direitos reservados</FooterText>
+      <FooterText>&copy; 2024 Ponto do Açaí - Todos os direitos reservados</FooterText>
       <SocialLinks>
         <SocialLink href="#facebook">Facebook</SocialLink>
         <SocialLink href="#instagram">Instagram</SocialLink>
-        <SocialLink href="#twitter">Twitter</SocialLink>
       </SocialLinks>
     </StyledFooter>
   );
