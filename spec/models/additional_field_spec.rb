372e6f9307a1d9ffc9c2139bd52b79cb1c381305
Adoniran <adoniranfranca@gmail.com>
2025-01-25 21:59:02 -0300

    adiciona teste de model para additional_field

--- spec/models/additional_field_spec.rb
+++ spec/models/additional_field_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe AdditionalField, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'update_order_total' do
+    context 'atualiza preço total do pedido' do
+      it 'ao adicionar adicional' do
+        order = create :order,  items_count: 1, items_price: 20
+        additional = create :additional_field, item: order.items.first, additional_value: 10.0
+
+
+        expect(additional.additional_value).to eq 10.0
+        expect(order.reload.total_price).to eq 30.0
+      end
+
+      it 'ao atualizar adicional' do
+        order = create :order,  items_count: 1, items_price: 20
+        additional = order.additional_fields.first
+
+        additional.update additional_value: 5
+
+        expect(additional.additional_value).to eq 5.0
+        expect(order.reload.total_price).to eq 25.0
+      end
+    end
+  end
 end
