3e318892f1600e809c1600bad5dcb9d08bd905e4
Adoniran <adoniranfranca@gmail.com>
2025-01-26 01:55:05 -0300

    adiciona teste de request para update de customer

---
+++ spec/requests/api/v1/customers/update_customer_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'PUT /api/v1/customers/:id', type: :request do
+  let(:user) { create(:user) }
+  let(:customer) { create(:customer, user:) }
+
+  it 'atualiza os dados de um cliente com sucesso' do
+    params = { customer: { name: 'Michael Updated' } }
+    put "/api/v1/customers/#{customer.id}", params: params
+
+    expect(response).to have_http_status(:ok)
+    json_response = JSON.parse(response.body)
+    expect(json_response['name']).to eq 'Michael Updated'
+  end
+
+  it 'retorna erro ao tentar atualizar com dados inválidos' do
+    params = { customer: { name: '' } }
+    put "/api/v1/customers/#{customer.id}", params: params
+
+    expect(response).to have_http_status(:unprocessable_entity)
+    json_response = JSON.parse(response.body)
+    expect(json_response['name']).to include('não pode ficar em branco')
+  end
+end
