2a1698fde016656bb33a31b227004a95499608eb
Adoniran <adoniranfranca@gmail.com>
2025-02-01 12:52:58 -0300

    adiciona garantias extras aos testes da action index de users

--- spec/requests/api/v1/users/index_users_spec.rb
+++ spec/requests/api/v1/users/index_users_spec.rb
@@ -4,14 +4,28 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   describe 'GET /api/v1/users' do
     let(:admin_user) { create(:user, :admin) }
 
-    it 'retorna os colaboradores com sucesso' do
+    it 'retorna 5 usuários por page com sucesso' do
+      create_list :user, 6, admin_id: admin_user.id
+
+      get :index, params: { admin_id: admin_user.id, page: 1 }
+
+      json_response = JSON.parse response.body
+      expect(response).to have_http_status :ok
+      expect(json_response['users'].length).to eq 5
+      expect(json_response['total_count']).to eq 7
+    end
+
+    it 'retorna apenas os colaboradores do admin' do
+      other_admin = create :user
       create_list :user, 3, admin_id: admin_user.id
+      create :user, admin_id: other_admin.id
 
       get :index, params: { admin_id: admin_user.id }
 
       json_response = JSON.parse response.body
       expect(response).to have_http_status :ok
       expect(json_response['users'].length).to eq 4
+      expect(json_response['total_count']).to eq 4
     end
 
     it 'aplica filtro de pesquisa por email' do
