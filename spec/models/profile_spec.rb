3608e797f1dfbb8c95fcffeb041d33a30508b293
Adoniran <adoniranfranca@gmail.com>
2025-02-06 03:24:44 -0300

    adiciona teste para o model Profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user_with_profile) }
+  let(:profile) { user.profile }
+
+  describe 'validações' do
+    it 'é válido com um nome completo' do
+      profile.full_name = 'João Silva'
+      expect(profile).to be_valid
+    end
+
+    it 'é inválido sem um nome completo' do
+      profile.full_name = nil
+      expect(profile).to be_invalid
+      expect(profile.errors[:full_name]).to include('não pode ficar em branco')
+    end
+
+    it 'é inválido com um formato de foto não suportado' do
+      profile.photo.attach(io: fixture_file_upload('spec/fixture/files/text_file.txt'),
+                           filename: 'text_file.txt')
+      expect(profile).to be_invalid
+      expect(profile.errors[:base]).to include('Apenas JPEG ou PNG são formatos aceitos')
+    end
+  end
+
+  describe '#photo_url' do
+    it 'retorna a URL correta da foto quando está anexada' do
+      profile.photo.attach(io: fixture_file_upload('spec/fixture/files/profile_image.jpg'),
+                           filename: 'profile_image.jpg')
+      expect(profile.photo_url).to include('/rails/active_storage/blobs/redirect')
+    end
+
+    it 'retorna nil se nenhuma foto estiver anexada' do
+      expect(profile.photo_url).to be_nil
+    end
+  end
 end
