76ad15642a8bde92e62c2fc1657e8c0535834d6f
Adoniran <adoniranfranca@gmail.com>
2025-02-14 14:44:12 -0300

    refatora Filterable

--- app/models/filterable.rb
+++ app/models/filterable.rb
@@ -1,7 +1,7 @@
 module Filterable
   extend ActiveSupport::Concern
 
-  class_methods do
+  module ClassMethods
     def filter_by_attributes(query, params)
       query = query&.downcase
       joins_tables, conditions, values = process_params(query, params)
@@ -33,33 +33,40 @@ module Filterable
 
     def process_relation(param, query, joins_tables, conditions, values)
       relation, column = param.split('.')
-      relation_sym = relation.to_sym
+      validate_relation(relation)
+      associated_table = reflect_on_association(relation.to_sym).klass.table_name
 
-      raise "Unknown relation: #{relation}" unless reflect_on_association(relation_sym)
-
-      associated_table = reflect_on_association(relation_sym).klass.table_name
-      joins_tables << relation_sym
+      joins_tables << relation.to_sym
       conditions << "LOWER(#{associated_table}.#{column}) LIKE ?"
       values << "%#{query}%"
+    end
+
+    def validate_relation(relation)
+      return if reflect_on_association(relation.to_sym)
+
+      raise "Unknown relation: #{relation}"
     end
 
     def process_attribute(param, query, conditions, values)
       if column_names.include?(param.to_s)
         column_type = columns_hash[param.to_s].type
-
-        conditions << if %i[string text].include?(column_type)
-                        "LOWER(#{table_name}.#{param}) LIKE ?"
-                      else
-                        "#{table_name}.#{param}::text LIKE ?"
-                      end
+        conditions << attribute_condition(param, column_type)
         values << "%#{query}%"
       else
         Rails.logger.warn "Column #{param} does not exist in the model #{name}"
       end
     end
 
+    def attribute_condition(param, column_type)
+      if %i[string text].include?(column_type)
+        "LOWER(#{table_name}.#{param}) LIKE ?"
+      else
+        "#{table_name}.#{param}::text LIKE ?"
+      end
+    end
+
     def apply_joins(joins_tables)
-      query_result = self
+      query_result = all
       joins_tables.uniq.each do |table|
         query_result = query_result.left_joins(table)
       end
